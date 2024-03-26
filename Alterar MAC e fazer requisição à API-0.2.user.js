b2e9868a3c55c67d0c238e63c998a1d580381512
wrGGsiq <164651948+wrGGsiq@users.noreply.github.com>
2024-03-26 22:06:43 +0000

    Add files via upload

---
+++ Alterar MAC e fazer requisição à API-0.2.user.js
@@ -0,0 +1,83 @@
+// ==UserScript==
+// @name         Alterar MAC e fazer requisição à API
+// @version      0.2
+// @description  Altera o MAC e faz uma requisição à API ao identificar a tabela específica
+// @author       Wesley GG
+// @match        https://integrator6.gegnet.com.br/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=gegnet.com.br
+// @grant        GM_xmlhttpRequest
+// ==/UserScript==
+
+
+
+function fetchVendor(macAddress, macCell) {
+    const url = "https://api.macvendors.com/" + encodeURIComponent(macAddress);
+    GM_xmlhttpRequest({
+        method: "GET",
+        url: url,
+        onload: function(response) {
+            if (response.status === 200) {
+                const vendor = response.responseText;
+
+                const currentText = macAddress + " (" + vendor + ")";
+                if (macCell.textContent.trim() !== currentText) {
+                    macCell.textContent = currentText;
+                    return currentText;
+                }
+            } else {
+              return 1;
+            }
+        }
+    });
+}
+
+async function modifyTable() {
+
+    let table = document.querySelectorAll('.ui-table-tbody')[1];
+    let macRow;
+    let macCell;
+
+
+    if (!table) {
+        return 1;
+    }else{
+        macRow = table.querySelectorAll('tr');
+        let temp = 0;
+        for (const element of macRow){
+            let temp_td = 0;
+            for (const tag_td of element.querySelectorAll('td')){
+                let i = tag_td.textContent.trim()
+                if (i.indexOf("Last-Mac") !== -1){
+                    macCell = element.querySelectorAll('td')[temp_td+1];
+                }
+            temp_td += 1;
+            }
+            temp += 1;
+        }
+        if (macCell){
+            const macAddress = macCell.textContent.trim();
+
+            const hasVendorText = macAddress.indexOf("(")
+            if (hasVendorText !== -1) {
+                return 1;
+            }else{
+                fetchVendor(macAddress, macCell);
+                return 1;
+            }
+        }
+
+    }
+}
+
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function executeRepeatedly() {
+  while (true) {
+    modifyTable();
+    await delay(100);
+  }
+}
+
+executeRepeatedly();
