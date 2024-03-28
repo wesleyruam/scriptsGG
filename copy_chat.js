37ac3ad344f6e7e1fff328c132ed49d10eadc4dd
wrGGsiq <164651948+wrGGsiq@users.noreply.github.com>
2024-03-27 22:57:04 -0300

    Create copy_chat.js

---
+++ copy_chat.js
@@ -0,0 +1,57 @@
+// ==UserScript==
+// @name         Copiar mensagens
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Copiar mensagens do chat para a área de transferência
+// @author       You
+// @match        https://ggnet.sz.chat/*
+// @grant        GM_setClipboard
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Função para extrair e formatar as mensagens do chat
+    function copiarMensagens() {
+        var liElements = document.querySelectorAll('.speech-wrapper li');
+        var mensagens = "";
+
+        liElements.forEach(function(li) {
+            var nome = li.querySelector('.name').textContent.trim();
+            var horario = li.querySelector('.timestamp').textContent.trim();
+            var texto = li.querySelector(".bubble > div:nth-child(2) > div > div:nth-child(1) > span").textContent.trim();
+
+            mensagens += nome + " - " + horario + " - " + texto + "\n";
+        });
+
+        return mensagens;
+    }
+
+    // Função para criar e adicionar o botão à div
+    function adicionarBotao() {
+        var divTagsSessions = document.querySelector('.item.tags-sessions');
+
+        if (divTagsSessions) {
+            var button = document.createElement('button');
+            button.textContent = 'Copiar Mensagens';
+            button.style.border = '1px solid black';
+            button.addEventListener('click', function() {
+                copiarParaAreaDeTransferencia();
+            });
+
+            divTagsSessions.appendChild(button);
+        }
+    }
+
+    // Copiar mensagens para a área de transferência
+    function copiarParaAreaDeTransferencia() {
+        var mensagens = copiarMensagens();
+        GM_setClipboard(mensagens);
+        console.log("Mensagens copiadas para a área de transferência:");
+        console.log(mensagens);
+    }
+
+    // Adicionar botão à div
+    adicionarBotao();
+
+})();
