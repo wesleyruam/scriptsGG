2a85b0bfcae41ea1da83fe4736ce374aee09e4a9
wrGGsiq <164651948+wrGGsiq@users.noreply.github.com>
2024-03-31 23:58:17 +0000

    Add files via upload

---
+++ Cria Relatorio Roteadores Huawei Comuns AX2 WS.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         Cria Relatorio Roteadores Huawei Comuns AX2 WS
+// @namespace    http://tampermonkey.net/
+// @version      0.5
+// @description  try to take over the world!
+// @author       Wesley GG
+// @match        https://*/html/index.html
+// @license      MIT
+// @grant        GM_setClipboard
+// ==/UserScript==
