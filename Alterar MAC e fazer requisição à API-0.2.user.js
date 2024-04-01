71a9d0a126dfb4f16fd70d3f47f5ec4ec7d803eb
wrGGsiq <164651948+wrGGsiq@users.noreply.github.com>
2024-03-31 21:05:27 -0300

    Update Alterar MAC e fazer requisição à API-0.2.user.js

--- Alterar MAC e fazer requisição à API-0.2.user.js
+++ Alterar MAC e fazer requisição à API-0.2.user.js
@@ -7,7 +7,7 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=gegnet.com.br
 // @license      MIT
 // @downloadURL  https://github.com/wrGGsiq/scriptsGG/raw/main/Alterar%20MAC%20e%20fazer%20requisi%C3%A7%C3%A3o%20%C3%A0%20API-0.2.user.js
-// @updateURL    https://github.com/wrGGsiq/scriptsGG/raw/main/Alterar%20MAC%20e%20fazer%20requisi%C3%A7%C3%A3o%20%C3%A0%20API-0.2.user.js
+// @updateURL    https://github.com/wrGGsiq/scriptsGG/raw/main/updates/Alterar%20MAC%20e%20fazer%20requisi%C3%A7%C3%A3o%20%C3%A0%20API-0.2.meta.js
 // @grant        GM_xmlhttpRequest
 // ==/UserScript==
 
