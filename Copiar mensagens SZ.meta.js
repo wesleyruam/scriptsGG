a2ec91abba46e539f30fb7f81d611d60454939f4
wrGGsiq <164651948+wrGGsiq@users.noreply.github.com>
2024-04-01 00:01:48 +0000

    Add files via upload

---
+++ Copiar mensagens SZ.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         Copiar mensagens SZ
+// @namespace    http://tampermonkey.net/
+// @version      0.4
+// @description  Copiar mensagens do chat do SZ para a área de transferência
+// @author       Wesley GG
+// @match        https://ggnet.sz.chat/*
+// @license      MIT
+// @grant        GM_setClipboard
+// ==/UserScript==
