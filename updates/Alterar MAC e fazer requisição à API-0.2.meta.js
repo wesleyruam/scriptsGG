149004354ca5fc22ce44ca542310679ebc695ff9
wrGGsiq <164651948+wrGGsiq@users.noreply.github.com>
2024-04-01 00:04:24 +0000

    Add files via upload

---
+++ updates/Alterar MAC e fazer requisição à API-0.2.meta.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         Alterar MAC e fazer requisição à API
+// @version      0.2
+// @description  Altera o MAC e faz uma requisição à API ao identificar a tabela específica
+// @author       Wesley GG
+// @match        https://integrator6.gegnet.com.br/*
+// @license      MIT
+// @grant        GM_xmlhttpRequest
+// ==/UserScript==
