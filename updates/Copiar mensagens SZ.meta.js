8e58c7e23945a2feb7880d94c15b8ceadad50323
wrGGsiq <164651948+wrGGsiq@users.noreply.github.com>
2024-04-04 20:36:31 -0300

    Update Copiar mensagens SZ.meta.js

--- updates/Copiar mensagens SZ.meta.js
+++ updates/Copiar mensagens SZ.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Copiar mensagens SZ
 // @namespace    http://tampermonkey.net/
-// @version      0.4
+// @version      0.5
 // @description  Copiar mensagens do chat do SZ para a área de transferência
 // @author       Wesley GG
 // @match        https://ggnet.sz.chat/*
