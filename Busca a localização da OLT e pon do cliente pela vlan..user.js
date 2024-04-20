073755f15017713ae041020c4a7c5607a5ab9563
wrGGsiq <164651948+wrGGsiq@users.noreply.github.com>
2024-04-20 14:49:56 -0300

    Update Busca a localização da OLT e pon do cliente pela vlan..user.js

--- Busca a localização da OLT e pon do cliente pela vlan..user.js
+++ Busca a localização da OLT e pon do cliente pela vlan..user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Busca a localização da OLT e pon do cliente pela vlan
-// @version      0.1
+// @version      0.2
 // @description  Busca a localização da OLT e PON do clinte pela vlan
 // @author       Wesley GG
 // @match        https://integrator6.gegnet.com.br/*
