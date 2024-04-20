f232a96613037a9c9c35a58f9e2c384611b7a64c
wrGGsiq <164651948+wrGGsiq@users.noreply.github.com>
2024-04-20 20:38:39 -0300

    Create Documentação ONU.meta.js

---
+++ updates/Documentação ONU.meta.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         Documentação ONU
+// @version      0.1
+// @description  Extrair automaticamente os dados da ONU do gCommit.
+// @author       Wesley GG
+// @match        https://ggnet.gcommit.com.br/showonu/*/*/*/*
+// @license      MIT
+// @grant        GM_setClipboard
+// ==/UserScript==
