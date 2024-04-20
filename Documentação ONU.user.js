af3269264429c0834912426e7ab25a7df1259652
wrGGsiq <164651948+wrGGsiq@users.noreply.github.com>
2024-04-20 20:39:27 -0300

    Update Documentação ONU.user.js

--- Documentação ONU.user.js
+++ Documentação ONU.user.js
@@ -4,6 +4,8 @@
 // @description  Extrair automaticamente os dados da ONU do gCommit.
 // @author       Wesley GG
 // @match        https://ggnet.gcommit.com.br/showonu/*/*/*/*
+// @downloadURL  https://github.com/wrGGsiq/scriptsGG/raw/main/Documenta%C3%A7%C3%A3o%20ONU.user.js
+// @updateURL    https://github.com/wrGGsiq/scriptsGG/raw/main/updates/Copiar%20mensagens%20SZ.meta.js
 // @grant        GM_setClipboard
 // ==/UserScript==
 
