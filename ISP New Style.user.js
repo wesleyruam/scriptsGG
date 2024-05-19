7b0c6ea1d8315f8af369cac24332175f9a9b1490
wrGGsiq <164651948+wrGGsiq@users.noreply.github.com>
2024-05-18 22:30:11 -0300

    Update ISP New Style.user.js

--- ISP New Style.user.js
+++ ISP New Style.user.js
@@ -8,6 +8,8 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=gegnet.com.br
 // @require      https://code.jquery.com/jquery-3.6.0.min.js
 // @require      https://code.jquery.com/ui/1.12.1/jquery-ui.js
+// @downloadURL  https://github.com/wrGGsiq/scriptsGG/raw/main/ISP%20New%20Style.user.js
+// @updateURL    https://github.com/wrGGsiq/scriptsGG/raw/main/updates/ISP%20New%20Style.meta.js
 // @grant        none
 // ==/UserScript==
 
