e201fac13349543b01e6756d8fc6d8d8c3e294c8
wrGGsiq <164651948+wrGGsiq@users.noreply.github.com>
2024-05-18 22:32:41 -0300

    Update ISP New Style.user.js

--- ISP New Style.user.js
+++ ISP New Style.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ISP New Style!
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://integrator6.gegnet.com.br/*
