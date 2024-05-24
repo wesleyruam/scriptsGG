c14d6ee718e1fcbe616a78aefeb10b0830fb74b9
wrGGsiq <164651948+wrGGsiq@users.noreply.github.com>
2024-05-24 16:30:26 -0300

    Update Notifica Pausa.meta.js

--- updates/Notifica Pausa.meta.js
+++ updates/Notifica Pausa.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Notifica Pausa
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      0.2
 // @description  A função deste script é lembrar o colaborador de que ele está em pausa, ja observei muitos casos na qual o colaborador coloca em pausa sai, e quando volta esquece de sair da pausa! Isso faz com que ele seja lembrado de sair da pausa.
 // @author       Wesley GG
 // @match        *://qm.coger.net.br:8080/queuemetrics/*
