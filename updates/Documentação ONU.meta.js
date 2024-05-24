2c1dcdd17b58cc30fc5db44304441430ad503b9f
wrGGsiq <164651948+wrGGsiq@users.noreply.github.com>
2024-05-24 14:50:27 -0300

    Update Documentação ONU.meta.js

--- updates/Documentação ONU.meta.js
+++ updates/Documentação ONU.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Documentação ONU
-// @version      0.2
+// @version      0.5
 // @description  Extrair automaticamente os dados da ONU do gCommit.
 // @author       Wesley GG
 // @match        https://ggnet.gcommit.com.br/showonu/*/*/*/*
