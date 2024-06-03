17350885e79bf9040f4b22e4f7da0761674bb230
wrGGsiq <164651948+wrGGsiq@users.noreply.github.com>
2024-06-03 19:17:45 -0300

    Update Mostra agendados NOC e SZ no ISP.user.js

--- Mostra agendados NOC e SZ no ISP.user.js
+++ Mostra agendados NOC e SZ no ISP.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         Mostra agendados NOC e SZ no ISP
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  Mostra agendados NOC e SZ no ISP
 // @author       You
 // @match        https://integrator6.gegnet.com.br/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=net.br
 // @downloadURL     https://github.com/wrGGsiq/scriptsGG/raw/main/Mostra%20agendados%20NOC%20e%20SZ%20no%20ISP.user.js
-// @uploadURL       https://github.com/wrGGsiq/scriptsGG/raw/main/Mostra%20agendados%20NOC%20e%20SZ%20no%20ISP.user.js
+// @updateURL       https://github.com/wrGGsiq/scriptsGG/raw/main/Mostra%20agendados%20NOC%20e%20SZ%20no%20ISP.user.js
 // @grant        GM_xmlhttpRequest
 // @grant        GM_addStyle
 // ==/UserScript==
