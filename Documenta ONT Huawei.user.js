72b8d94f8e0a58b578da9c1d6fd83e6773a7a691
Matheus Eger <87047768+MatheusEger@users.noreply.github.com>
2024-06-21 17:30:12 -0300

    Update Documenta ONT Huawei.user.js

--- Documenta ONT Huawei.user.js
+++ Documenta ONT Huawei.user.js
@@ -2,7 +2,7 @@
 // @name         Documenta ONT Huawei (versão de testes)
 // @namespace    http://tampermonkey.net/
 // @version      Demo
-// @description  Extrai informações necessario para documentação da ONT clicando no nome da ONT, canto esquerdo superior.
+// @description  Extrai informações necessario para documentação da ONT clicando no nome da ONT (Ex: EG8145X6-10), canto esquerdo superior.
 // @author       Matheus Eger
 // @match        *://*/index.asp
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=www.huawei.com
