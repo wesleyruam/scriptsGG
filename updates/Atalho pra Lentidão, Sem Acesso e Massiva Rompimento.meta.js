297d7fd04311f97a319cfa82e412568541390e7e
Matheus Eger <87047768+MatheusEger@users.noreply.github.com>
2024-07-01 14:53:22 -0300

    Update Atalho pra Lentidão, Sem Acesso e Massiva Rompimento.meta.js

--- updates/Atalho pra Lentidão, Sem Acesso e Massiva Rompimento.meta.js
+++ updates/Atalho pra Lentidão, Sem Acesso e Massiva Rompimento.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Atalho pra Lentidão, Sem Acesso e Massiva Rompimento
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.2
 // @description  Script focado para automatizar o processo de mundança de categoria, tipo e motivo dos atendimento. Implementalçao incial de 3 botões na parte do atendimento.
 // @author       Matheus Eger
 // @match        https://integrator6.gegnet.com.br/*
