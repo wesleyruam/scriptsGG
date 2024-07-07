86401c5d927870d38c7db17bd68ffd9349aefad8
wrGGsiq <164651948+wrGGsiq@users.noreply.github.com>
2024-07-07 16:15:31 -0300

    Update baixar mensagens em pdf.user.js

--- baixar mensagens em pdf.user.js
+++ baixar mensagens em pdf.user.js
@@ -6,6 +6,8 @@
 // @author       Wesley GG
 // @match        https://ggnet.sz.chat/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=gegnet.com.br
+// @downloadURL  https://github.com/wrGGsiq/scriptsGG/raw/main/baixar%20mensagens%20em%20pdf.user.js
+// @updateURL    https://github.com/wrGGsiq/scriptsGG/raw/main/baixar%20mensagens%20em%20pdf.user.js
 // @grant        none
 // ==/UserScript==
 
