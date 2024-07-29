463b5d8efa8e5ce35a4f7fab91b7ae874b3b3c18
wrGGsiq <164651948+wrGGsiq@users.noreply.github.com>
2024-07-29 15:21:25 -0300

    Update Logs ISP Radius.user.js

--- Logs ISP Radius.user.js
+++ Logs ISP Radius.user.js
@@ -5,8 +5,8 @@
 // @author       Wesley GG
 // @match        https://integrator6.gegnet.com.br/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=gegnet.com.br
-// @downloadURL
-// @uploadURL
+// @downloadURL  https://github.com/wrGGsiq/scriptsGG/raw/main/Logs%20ISP%20Radius.user.js
+// @uploadURL    https://github.com/wrGGsiq/scriptsGG/raw/main/Logs%20ISP%20Radius.user.js
 // @grant        GM_addStyle
 // @grant        GM_xmlhttpRequest
 // @grant        GM_addElement
