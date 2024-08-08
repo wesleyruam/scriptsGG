6153da3878a20364c4228e64a9eba10dd3742312
Matheus Eger <87047768+MatheusEger@users.noreply.github.com>
2024-08-08 16:46:29 -0300

    Update logarnafilasz.meta.js

--- updates/logarnafilasz.meta.js
+++ updates/logarnafilasz.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         LOGAR e DESLOGAR DA FILA - SZ.CHAT
-// @version      3.1
+// @version      3.2
 // @description  Automação para logar na Fila
 // @author       Matheus Eger
 // @match        http://qm.coger.net.br:8080/queuemetrics/qm_agentpage2_load.do
